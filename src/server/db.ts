f4aa464ce684162e2807ba71a77df3e6f9f4da91
Your Name <you@example.com>
2022-11-15 19:02:12 +0100

    Connect sqlite3 database

---
+++ src/server/db.ts
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const dbPath: string = path.resolve(__dirname, "db/database.sqlite");
+
+const knex = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: dbPath,
+  },
+  useNullAsDefault: true,
+});
+
+module.exports = knex;
