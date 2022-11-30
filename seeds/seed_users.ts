d21a9acb0dd514a9eccad14d37376308006dc956
Your Name <you@example.com>
2022-11-30 10:55:36 +0100

    Remove id property (unnecessary) in user declaration, knex insert one after insert users. Create dummy ratings array, insert ratings to mobiles. Using Services we can access to user_Id and mobiles_id and set ratings

--- seeds/seed_users.ts
+++ seeds/seed_users.ts
@@ -1,25 +1,29 @@
 import { Knex } from "knex";
+import { IMobileSeed } from "../src/server/types/types";
+const mobileService = require("../src/server/models/mobileService");
+const userService = require("../src/server/models/userService");
 
 export async function seed(knex: Knex): Promise<void> {
-  // Deletes ALL existing entries
   await knex("users").del();
   await knex("mobiles").del();
+  await knex("rating").del();
 
-  // Inserts seed entries
-  await knex("users").insert([
-    { id: 1, name: "Joe" },
-    { id: 2, name: "Charles" },
-    { id: 3, name: "Ana" },
-  ]);
-  const apple = { id: 1, brand: "Apple", model: "10" };
-  const apple2 = { id: 2, brand: "Apple", model: "12" };
-  const apple3 = { id: 3, brand: "Apple", model: "14" };
-  const samsung = { id: 4, brand: "Samsung", model: "Galaxy" };
-  const samsung2 = { id: 5, brand: "Samsung", model: "Galaxy N" };
-  const samsung3 = { id: 6, brand: "Samsung", model: "Galaxy S" };
-  const xiaomi = { id: 7, brand: "Xiomi", model: "Xv3" };
-  const xiaomi2 = { id: 8, brand: "Xiomi", model: "Xv6" };
-  // Inserts seed entries
+  let joe: any = { name: "Joe" };
+  let charles: any = { name: "Charles" };
+  let ana: any = { name: "Ana" };
+  let betty: any = { name: "Betty" };
+
+  await knex("users").insert([joe, charles, ana, betty]);
+
+  let apple: IMobileSeed = { brand: "Apple", model: "10" };
+  let apple2: IMobileSeed = { brand: "Apple", model: "12" };
+  let apple3: IMobileSeed = { brand: "Apple", model: "14" };
+  let samsung: IMobileSeed = { brand: "Samsung", model: "Galaxy" };
+  let samsung2: IMobileSeed = { brand: "Samsung", model: "Galaxy N" };
+  let samsung3: IMobileSeed = { brand: "Samsung", model: "Galaxy S" };
+  let xiaomi: IMobileSeed = { brand: "Xiaomi", model: "Xv3" };
+  let xiaomi2: IMobileSeed = { brand: "Xiaomi", model: "Xv6" };
+
   await knex("mobiles").insert([
     apple,
     apple2,
@@ -30,4 +34,43 @@ export async function seed(knex: Knex): Promise<void> {
     xiaomi,
     xiaomi2,
   ]);
+
+  joe = await userService.findUserByName(joe.name);
+  charles = await userService.findUserByName(charles.name);
+  ana = await userService.findUserByName(ana.name);
+  betty = await userService.findUserByName(betty.name);
+
+  apple = await mobileService.findMobile(apple.brand, apple.model);
+  apple2 = await mobileService.findMobile(apple2.brand, apple2.model);
+  apple3 = await mobileService.findMobile(apple3.brand, apple3.model);
+  samsung = await mobileService.findMobile(samsung.brand, samsung.model);
+  samsung2 = await mobileService.findMobile(samsung2.brand, samsung2.model);
+  samsung3 = await mobileService.findMobile(samsung3.brand, samsung3.model);
+  xiaomi = await mobileService.findMobile(xiaomi.brand, xiaomi.model);
+  xiaomi2 = await mobileService.findMobile(xiaomi2.brand, xiaomi2.model);
+
+  const ratings = [
+    { rate: 6, mobile_id: apple.id, user_id: joe.id },
+    { rate: 8, mobile_id: apple.id, user_id: ana.id },
+    { rate: 8, mobile_id: apple.id, user_id: betty.id },
+    { rate: 4, mobile_id: apple2.id, user_id: charles.id },
+    { rate: 7, mobile_id: apple2.id, user_id: joe.id },
+    { rate: 3, mobile_id: apple3.id, user_id: ana.id },
+    { rate: 8, mobile_id: apple3.id, user_id: betty.id },
+    { rate: 6, mobile_id: apple3.id, user_id: joe.id },
+    { rate: 7, mobile_id: samsung.id, user_id: charles.id },
+    { rate: 9, mobile_id: samsung.id, user_id: betty.id },
+    { rate: 6, mobile_id: samsung2.id, user_id: ana.id },
+    { rate: 5, mobile_id: samsung2.id, user_id: joe.id },
+    { rate: 7, mobile_id: samsung2.id, user_id: charles.id },
+    { rate: 6, mobile_id: samsung3.id, user_id: ana.id },
+    { rate: 5, mobile_id: samsung3.id, user_id: charles.id },
+    { rate: 3, mobile_id: samsung3.id, user_id: betty.id },
+    { rate: 9, mobile_id: xiaomi.id, user_id: joe.id },
+    { rate: 10, mobile_id: xiaomi.id, user_id: charles.id },
+    { rate: 8, mobile_id: xiaomi2.id, user_id: charles.id },
+    { rate: 6, mobile_id: xiaomi2.id, user_id: betty.id },
+  ];
+
+  await knex("rating").insert(ratings);
 }
